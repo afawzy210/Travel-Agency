ed40e4e68a11aa0caec38eb273f1c8e6f3f81032
afawzy210 <100765824+afawzy210@users.noreply.github.com>
2022-05-19 00:39:21 +0200

    .

--- TravelAgency/TravelAgency/Customer.cs
+++ TravelAgency/TravelAgency/Customer.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TravelAgency
 {
-    public partial class Form1 : Form
+    public partial class Customer : Form
     {
-        public Form1()
+        public Customer()
         {
             InitializeComponent();
         }
